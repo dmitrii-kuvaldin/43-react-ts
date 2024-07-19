9934d712cc438e389d8e40e03dcc1f90c49cb7b4
mohnapne <kuvaldin2012@gmail.com>
2024-07-19 12:32:08 +0200

    routing

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,13 +1,20 @@
+import { Link, Outlet, useLocation } from 'react-router-dom';
 import styles from './layout.module.css'
 
 function Layout() {
+  const location = useLocation()
+  console.log(location.pathname);
   return (
     <div className={styles.page}>
       <header className={styles.header}>
-        header
+        <Link className={location.pathname === '/' ? styles.active : ''} to={'/'}>star wars gallery</Link>
+        <Link className={location.pathname === '/gender-form' ? styles.active : ''} to={'/gender-form'}>gender form</Link>
+        <Link className={location.pathname === '/robot-form' ? styles.active : ''} to={'/robot-form'}>robot form</Link>
+        <Link className={location.pathname === '/my-form' ? styles.active : ''} to={'/my-form'}>form example</Link>
       </header>
       <main className={styles.main}>
-        
+        {/* на место outlet будут приходить компоненты из вложенных route */}
+        <Outlet/>
       </main>
       <footer className={styles.footer}>
         footer
--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,29 @@
 import ReactDOM from 'react-dom/client';
+import { HashRouter, Route, Routes } from 'react-router-dom';
+import FormGender from './components/formGender/FormGender';
+import Layout from './components/layout/Layout';
+import StarWarsGallery from './components/starWarsGallery/StarWarsGallery';
 import './index.css';
-import Lesson13 from './lessons/lesson13/Lesson13';
+import RobotForm from './components/robotForm/RobotForm';
+import MyForm from './components/myForm/MyForm';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <>
-    {/* <App /> */}
-    {/* <Lesson07/> */}
-    {/* <Lesson08 /> */}
-    {/* <Layout /> */}
-    {/* <StarWarsGallery /> */}
-    {/* <Lesson09 /> */}
-    {/* <Lesson11/> */}
-    {/* <Lesson12 /> */}
-    <Lesson13/>
-  </>
+  // обертка router над всеми элементами
+  <HashRouter>
+    {/* обертка для описания маршрутов */}
+    <Routes>
+      {/* маршрут-родитель в котором мы будем отображать дочерние компоненты */}
+      <Route path='/' element={<Layout />} >
+        <Route path='/' element={<StarWarsGallery />} />
+        <Route path='/gender-form' element={<FormGender />} />
+        <Route path='/robot-form' element={<RobotForm />} />
+        <Route path='/my-form' element={<MyForm />} />
+      </Route>
+    </Routes>
+  </HashRouter>
 );
 
 
